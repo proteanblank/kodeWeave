951699294be61e8ba2e4e91e7d5a845afda547b5
Michael Schwartz <mikethedj4@yahoo.com>
2016-05-04 14:53:26 -0500

    Updated Codbmirror

--- editor/libraries/codemirror/addon/lint/html-lint.js
+++ editor/libraries/codemirror/addon/lint/html-lint.js
@@ -7,21 +7,19 @@
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
+    mod(require("../../lib/codemirror"), require("htmlhint"));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
+    define(["../../lib/codemirror", "htmlhint"], mod);
   else // Plain browser env
     mod(CodeMirror);
-})
-
-(function(CodeMirror) {
+})(function(CodeMirror) {
   "use strict";
 
-  var ruleSets = {
+  var defaultRules = {
     "tagname-lowercase": true,
     "attr-lowercase": true,
     "attr-value-double-quotes": true,
-    "doctype-first": true,
+    "doctype-first": false,
     "tag-pair": true,
     "spec-char-escape": true,
     "id-unique": true,
@@ -29,13 +27,13 @@
     "attr-no-duplication": true
   };
 
-  CodeMirror.registerHelper("lint", "html", function(text) {
-    var found = [], message;
+  CodeMirror.registerHelper("lint", "html", function(text, options) {
+    var found = [];
     if (!window.HTMLHint) return found;
-    var messages = HTMLHint.verify(text, ruleSets);
-    for ( var i = 0; i < messages.length; i++) {
-      message = messages[i];
-      var startLine = message.line -1, endLine = message.line -1, startCol = message.col -1, endCol = message.col;
+    var messages = HTMLHint.verify(text, options && options.rules || defaultRules);
+    for (var i = 0; i < messages.length; i++) {
+      var message = messages[i];
+      var startLine = message.line - 1, endLine = message.line - 1, startCol = message.col - 1, endCol = message.col;
       found.push({
         from: CodeMirror.Pos(startLine, startCol),
         to: CodeMirror.Pos(endLine, endCol),
@@ -44,5 +42,5 @@
       });
     }
     return found;
-  }); 
+  });
 });
